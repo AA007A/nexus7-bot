b25c631f4e701a186fbad916287fd1d7432c2d7d
AA007A <airton32a@outlook.com>
2026-09-09 15:50:21 -0300

    Install macro news risk bridge

--- bot/runtime_bootstrap.py
+++ bot/runtime_bootstrap.py
@@ -40,6 +40,7 @@ def install() -> None:
     from bot import selfcheck as _selfcheck
     from bot import news_context_hardening as _news_context_hardening
     from bot import market_risk_runtime as _market_risk_runtime
+    from bot import market_risk_news_bridge as _market_risk_news_bridge
     from bot import balance_observability as _balance_observability
     from bot import account_balance_observability as _account_balance_observability
     from bot import stagnation_time_hardening as _stagnation_time_hardening
@@ -74,6 +75,7 @@ def install() -> None:
     _instrument_readiness_guard.install(_log)
     _news_context_hardening.install(_log)
     _market_risk_runtime.install(_pilot.PilotGuard, _score, _log)
+    _market_risk_news_bridge.install(_news_context_hardening, _market_risk_runtime, _log)
     _balance_observability.install(_log)
     _account_balance_observability.install(_log)
     _pilot_exposure_capacity.install(_pilot.PilotGuard, _log)
